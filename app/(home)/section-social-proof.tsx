713b74bbb75daa7a59f0ccb3a91019d5b9acd9ca
Lucien-Arb <lucienarbieulemenan@gmail.com>
2025-01-17 09:31:02 +0100

    landing: #38 page

--- app/(home)/section-social-proof.tsx
+++ app/(home)/section-social-proof.tsx
@@ -4,14 +4,12 @@ import Script from "next/script";
 
 const SectionSocialProof = () => {
   return (
-    <section className="w-full py-8 md:py-12 px-4 mt-12 md:mt-24 mb-12" id="testimonials">
+    <section className="w-full py-8 md:py-12 px-4 my-12 md:my-24" id="testimonials">
       <div className="max-w-6xl mx-auto">
-        <h2 className="font-bold text-center text-3xl sm:text-4xl text-title !leading-tight max-w-[550px] mx-auto mb-12 sm:mb-4">
+        <h2 className="font-bold text-center text-3xl sm:text-4xl text-title !leading-tight max-w-[550px] mx-auto mb-12 sm:mb-20">
           La communauté <br/>tech parle de nous.
         </h2>
-        
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 relative">
-        
+                
           <Script 
             src="https://widget.senja.io/widget/b9e26795-a8a0-461d-bdf2-8cfc11abaada/platform.js" 
             strategy="lazyOnload"
@@ -23,8 +21,6 @@ const SectionSocialProof = () => {
             data-lazyload="false" 
             style={{ display: 'block' }}
           />
-          
-        </div>
       </div>
     </section>
   );
