238cb9dcee659991fba7a1632d1a4545b319f3ad
Lucien-Arb <lucienarbieulemenan@gmail.com>
2025-02-13 20:28:25 +0100

    main

--- app/(home)/section-pricing.tsx
+++ app/(home)/section-pricing.tsx
@@ -9,20 +9,20 @@ const PricingSection = () => {
   const pricing = useTranslations("HomePage.Pricing");
 
   return (
-    <section className="w-full py-24 px-4 bg-popover">
+    <section className="w-full py-12 sm:py-24 px-4 bg-popover">
       <div className="max-w-[1120px] mx-auto">
-        <div className="flex flex-col items-center justify-center text-center text-muted mb-2">
-          <h2 className="text-3xl md:text-4xl text-title font-bold max-w-[350px] sm:max-w-[650px] mb-20">
+        <div className="flex flex-col items-center justify-center text-center text-muted mb-4">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl text-title font-bold max-w-[350px] sm:max-w-[650px] mb-10">
             {pricing("title.prefix")}
             <span className="text-primary">{pricing("title.highlight")}</span>
             {pricing("title.suffix")}
           </h2>
-          <Card className="overflow-hidden bg-white text-title scale-110 border-primary border-2 shadow-lg mb-12">
-            <div className="px-14 py-12 sm:px-10">
-              <h3 className="text-2xl font-bold mb-8">
+          <Card className="overflow-hidden bg-white text-title scale-110 border-primary border-2 shadow-lg mb-6">
+            <div className="px-6 sm:px-10 py-8">
+              <h3 className="text-xl sm:text-2xl font-bold mb-6">
                 {pricing("card.title").toUpperCase()} <span className="text-primary underline">{pricing("card.highlight").toUpperCase()}</span>
               </h3>
-              <ul className="space-y-4 text-muted-foreground mb-8">
+              <ul className="space-y-3 text-muted-foreground mb-6">
                 {pricing.raw("card.avantage").map((avantage: string, index: number) => (
                   <li key={index} className="flex items-center">
                     <Image
@@ -44,10 +44,10 @@ const PricingSection = () => {
               </button>
             </div>
           </Card>
-          <p className="text-sm sm:text-base mt-4">
+          <p className="text-xs sm:text-sm mt-2">
             {pricing("info-pricing")}
           </p>
-          <p className="text-sm sm:text-base mt-2">
+          <p className="text-xs sm:text-sm mt-1">
             {pricing("info-supply")}
           </p>
         </div>
